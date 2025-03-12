8ef38c51cf628694820267ea0b88a80cc11addb9
deoxce <deoxceofficial@gmail.com>
2025-03-12 23:36:32 +0500

    code improvements + react query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById('app')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </QueryClientProvider>
 );
