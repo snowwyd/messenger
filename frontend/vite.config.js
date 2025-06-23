67f819d24e56987074f2175e5c1954ff17cf1f1f
deoxce <deoxce.dev@gmail.com>
2025-06-23 16:13:46 +0500

    updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
     plugins: [react()],
